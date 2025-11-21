abd8a55eb7a0715c66e5a7c55a75e84153337a90
masangakevin256 <masangakevin1@gmail.com>
2025-11-21 21:57:47 +0300

    modifying whitelist

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-  "http://localhost:5173",       // your React frontend
-  "http://localhost:3500",
-  "http://localhost:3500" ,      // your production frontend
+  "http://localhost:5173",       // React frontend
+  "http://localhost:3500",     //  production frontend
   "https://kevinnyprojectmedicarehospital.vercel.app"
 ];
 module.exports= allowedOrigins;
